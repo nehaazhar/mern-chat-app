12f46e5257222be259775e28bd267824069d71d4
nehaazhar <70388009+nehaazhar@users.noreply.github.com>
2026-06-24 00:40:18 +0530

    Fix: Replace hardcoded localhost DB URL with process.env.MONGO_URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/MERN-Chat-App");
+    const conn = await mongoose.connect("process.env.MONGO_URI");
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
-    process.exit(); 
+    process.exit();
   }
 };
 
