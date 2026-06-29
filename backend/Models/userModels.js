0bec60725ea66b4824790e73128ca495daeb168d
nehaazhar <70388009+nehaazhar@users.noreply.github.com>
2026-06-29 22:40:18 +0530

    Add edit profile  feature in the App

--- backend/Models/userModels.js
+++ backend/Models/userModels.js
@@ -22,13 +22,18 @@ userSchema.methods.matchPassword = async function (enterPassword) {
   return await bcrypt.compare(enterPassword, this.password);
 };
 
-userSchema.pre("save", async function () {
+userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     return next();
   }
 
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
 
 // const User = mongoose.model("User", userSchema);
